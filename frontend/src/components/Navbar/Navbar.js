093d8b718fb352f32aa64f3fcd4839166f0d7916
Preethika Prasadi <preethikaprasadi@gmail.com>
2025-02-23 22:39:39 +0530

    Update nav bar

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.webp'; // Import the logo
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="FreshPress Logo" className="logo-image" />
+        </Link>
+        <div className="nav-links">
+          <Link to="/menu" className="nav-link">Menu</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/locations" className="nav-link">Locations</Link>
+          <Link to="/contact" className="nav-link">Contact</Link>
+          <Link to="/orderonline" className='nav-link'>
+          <button className="nav-cta">Order Online</button>
+          </Link>
+          <li><Link to="/orders">View Orders</Link></li>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
